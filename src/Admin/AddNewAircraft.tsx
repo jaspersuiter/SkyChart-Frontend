dc6599f1dc675ca521dcba8da3cc69e4bf4c2f42
Ares2028 <59334081+Ares2028@users.noreply.github.com>
2023-10-02 14:09:01 -0400

    added in the Add new Aircraft popup as well as all the appropriate accompening css. NOTE this is not very well done, and will probabley need to be fixed in the future.

---
+++ src/Admin/AddNewAircraft.tsx
@@ -0,0 +1,108 @@
+import { Dialog } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import TextField from '@mui/material/TextField';
+import './AddNewAircraft.css'
+import PrimaryButton from '../Buttons/PrimaryButton';
+import { useState } from 'react';
+import SecondaryButton from '../Buttons/SecondaryButton';
+
+export interface AddNewAircraftProp {
+    open: boolean;
+    onClose: () => void;
+  }
+function AddNewAircraft(props: AddNewAircraftProp){
+    const {open, onClose } = props;
+
+    const [tailNumber, setTailNumber] = useState('');
+    const [model, setModel] = useState('');
+    const [aircraftNickname, setAircraftNickname] = useState('');
+    const [numEngines, setNumEngines] = useState('');
+    const [tachHours, settachHours] = useState('');
+    const [hobbsHours, setHobbsHours] = useState('');
+    const [hourlyRate, setHourlyRate] = useState('');
+
+    const handleClose = () => {
+        onClose();
+      };
+
+      return (
+            <Dialog onClose={handleClose} open={open} sx={{
+                "& .MuiDialog-container": {
+                  "& .MuiPaper-root": {
+                    width: "100%",
+                    maxWidth: "900px",
+                    height: "100%",
+                    maxHeight: "650px",
+                    padding: "30px"
+                  },
+                },
+              }} >
+                <div className='maincontent'>
+                    <div className='top-title'>
+                        <div className='space-filler'></div>
+                        <h1>Add New Aircraft</h1>
+                        <div className='boxframe'><CloseIcon onClick={handleClose}/></div>
+                    </div>
+                    
+                    <div className='maindropdowncontent'>
+                        <div className='threewide'>
+                            <TextField id="Tail Number" 
+                            label="Tail Number" 
+                            value={tailNumber} 
+                            onChange={(event) => {setTailNumber(event.target.value)}}/>
+
+
+                            <TextField id="Model" 
+                            label="Model" 
+                            value={model} 
+                            onChange={(event) => {setModel(event.target.value)}}/>
+
+
+                            <TextField id="Aircraft Nickname" 
+                            label="Aircraft Nickname" 
+                            value={aircraftNickname} 
+                            onChange={(event) => {setAircraftNickname(event.target.value)}}/>
+                        </div>
+                        <div className='threewide'>
+                            <TextField id="Number of Engines" 
+                            label="Number of Engines" 
+                            value={numEngines} 
+                            onChange={(event) => {setNumEngines(event.target.value)}}/>
+
+
+                            <TextField id="Tach hours" 
+                            label="Tach Hours" 
+                            value={tachHours} 
+                            onChange={(event) => {settachHours(event.target.value)}}/>
+
+
+                            <TextField id="Hobbs Hours" 
+                            label="Hobbs Hours" 
+                            value={hobbsHours} 
+                            onChange={(event) => {setHobbsHours(event.target.value)}}/>
+                        </div>
+                        <div className='twowide'>
+                            <TextField id="Hourly Rate" 
+                            label="Hourly Rate" 
+                            value={hourlyRate} 
+                            onChange={(event) => {setHourlyRate(event.target.value)}}/>
+
+                            <SecondaryButton text="Aircraft Image"/>
+                        </div>
+                    </div>
+
+                    <div className='spacertwo'>
+
+                    </div>
+
+                    <div className='confirmationbottom'>
+                        <PrimaryButton text="Create Aircraft"/>
+                    </div>
+                </div>
+
+                
+            </Dialog>
+    );
+
+}
+export default AddNewAircraft
--- src/Admin/Admin.tsx
+++ src/Admin/Admin.tsx
@@ -1,8 +1,20 @@
 import PrimaryButton from "../Buttons/PrimaryButton";
 import StaticSidebar from "../Sidebar/Sidebar";
+import AddNewAircraft from "./AddNewAircraft";
+import React from 'react';
 import './Admin.css'
 
 function Admin() {
+
+    const [open, setOpenAddAircraft] = React.useState(false);
+
+    const handleClickOpenAddAircraft = () => {
+        setOpenAddAircraft(true);
+    };
+
+    const handleCloseAddAircraft = () => {
+        setOpenAddAircraft(false);
+    };
 
 
     return(
@@ -10,7 +22,12 @@ function Admin() {
         <div className="mainpage">
             <StaticSidebar/>
             <div className="main-content">
-                <PrimaryButton text="Add New Aircraft"/>
+                <PrimaryButton text="Add New Aircraft" onClick={handleClickOpenAddAircraft}/>
+
+                <AddNewAircraft 
+                    open={open}
+                    onClose={handleCloseAddAircraft}
+                />
             </div>
         </div>
     );
