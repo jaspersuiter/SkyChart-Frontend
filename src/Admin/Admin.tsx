3c2aac6c23a019092e44be1cb8b892839e52a2ea
Ares2028 <59334081+Ares2028@users.noreply.github.com>
2023-10-02 10:48:14 -0400

    Added in a basic admin Page at the bottom of the side bare, will need to be removed later

---
+++ src/Admin/Admin.tsx
@@ -0,0 +1,18 @@
+import StaticSidebar from "../Sidebar/Sidebar";
+import './Admin.css'
+
+function Admin() {
+
+
+    return(
+
+        <div className="mainpage">
+            <StaticSidebar/>
+            <div>
+                <p>Hello, World!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Admin;
