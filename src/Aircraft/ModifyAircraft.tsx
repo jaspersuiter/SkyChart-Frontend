7cd562c7b03c1f8183a4ada10a5cfba1ee908a8a
Bmeshanko <bmeshank@purdue.edu>
2023-10-16 15:06:29 -0400

    Messing with CSS

--- src/Aircraft/ModifyAircraft.tsx
+++ src/Aircraft/ModifyAircraft.tsx
@@ -1,14 +1,20 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthorizationContext } from "../AuthContext";
-import { Dialog } from "@mui/material";
+import { Dialog, TextField } from "@mui/material";
 
 export interface ModifyAircraftProps {
     open: boolean;
     onClose: () => void;
-  }
+}
 
 function ModifyAircraft(props: ModifyAircraftProps) {
+
+    const [nickname, setNickname] = useState('');
+    const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNickname(e.target.value);
+    }
+
     const context = useContext(AuthorizationContext);
     const navigate = useNavigate();
 
@@ -27,9 +33,14 @@ function ModifyAircraft(props: ModifyAircraftProps) {
     };
 
     return (
-        <div>
+        <div className="modify-aircraft-popup">
             <Dialog onClose={handleClose} open={open}>
-                
+                <div className="modify-aircraft-popup-content">
+                    <h1>Set a Nickname</h1>
+                    <form>
+                        <TextField id="nickname" label="Nickname" type="name" value={nickname} onChange={handleNicknameChange} />
+                    </form>
+                </div>
             </Dialog>
         </div>
     );
