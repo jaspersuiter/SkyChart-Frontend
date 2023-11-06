d41afaca6ecb7ddeadd692fdd4df23358d4566d2
Bmeshanko <bmeshank@purdue.edu>
2023-11-05 22:31:44 -0500

    Aircraft Page aesthetics

--- src/Aircraft/Aircraft.tsx
+++ src/Aircraft/Aircraft.tsx
@@ -1,53 +1,52 @@
 import './Aircraft.css';
 import '../App.css';
 import StaticSidebar from '../Utils/Sidebar/Sidebar';
-import { Box, Button, ButtonGroup, Grid, Paper, Typography } from '@mui/material';
-import { SetStateAction, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import SecondaryButton from '../Utils/Buttons/SecondaryButton';
-import React from 'react';
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 import ModifyAircraft from './ModifyAircraft/ModifyAircraft';
 import AircraftPopup from './AircraftPopup/AircraftPopup';
-import { Instructor, Plane } from '../Calendar/Calendar';
 import AddSqauwkPopup from './AddSquawkPopup/AddSquawkPopup';
 import NewReservation from '../Reservation/NewReservation/NewReservation';
+import { Instructor, Plane } from '../Calendar/Calendar';
 
 function Aircraft() {
-
-  const [open, setOpen] = React.useState(false);
-  const [openSquawk, setOpenSquawk] = React.useState(false);
-  const [openCreateReservation, setOpenCreateReservation] = React.useState(false);
-  const [openModify, setOpenModify] = React.useState(false);
-  const [currentPlane, setCurrentPlane] = React.useState<Plane>({} as Plane);
-  const [updateScreen, setUpdateScreen] = React.useState(false);
-
-  const handleUpdateScreen = () => {
-    setUpdateScreen(!updateScreen);
-  }
-
-  const [updateAircraftScreen, setUpdateAircraftScreen] = React.useState(false);
-
-    const handleUpdateAircraftScreen = () => {
-        setUpdateAircraftScreen(!updateAircraftScreen);
-    }
-
-
-  const handleClickOpen = (plane: any) => {
+  // Aircraft Popup
+  const [openAircraftPage, setOpenAircraftPage] = useState(false);
+  const [currentPlane, setCurrentPlane] = useState<Plane>({} as Plane);
+  
+  const handleClickPlane = (plane: any) => {
     setCurrentPlane(plane);
-    setOpen(true);
+    setOpenAircraftPage(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleCloseAircraftPage = () => {
+    setOpenAircraftPage(false);
   };
-
+  
+  // Add Squawk Popup
+  const [openAddSquawk, setOpenAddSquawk] = useState(false);
+  
   const handleClickOpenSquawk = () => {
-    setOpenSquawk(true);
+    setOpenAddSquawk(true);
   };
+
   const handleCloseSquawk = () => {
-    setOpenSquawk(false);
+    setOpenAddSquawk(false);
   }
 
+  // Modify Plane Popup
+  const [openModifyPlane, setOpenModifyPlane] = useState(false);
+  const handleClickOpenModify = () => {
+    setOpenModifyPlane(true);
+  };
+
+  const handleCloseModify = () => {
+    setOpenModifyPlane(false);
+  }
+
+  // Create Reservation 
+  const [openCreateReservation, setOpenCreateReservation] = useState(false);
+  
   const handleClickOpenCreateReservation = () => {
     setOpenCreateReservation(true);
   };
@@ -56,41 +55,40 @@ function Aircraft() {
     setOpenCreateReservation(false);
   }
 
-  const handleClickOpenModify = () => {
-    setOpenModify(true);
-  };
-
-  const handleCloseModify = () => {
-    setOpenModify(false);
+  // Update Screens
+  const [updateScreen, setUpdateScreen] = useState(false);
+  const [updateAircraftScreen, setUpdateAircraftScreen] = useState(false);
+  const handleUpdateScreen = () => {
+    setUpdateScreen(!updateScreen);
   }
 
+  // Plane Data Retrieval and Storage
   const [planes, setPlanes] = useState<Plane[]>([]);
-  const [instructors, setInstructors] = useState<Instructor[]>([]);
-
-  
   const fetchPlanes = async () => {
     try {
-        const planes = await fetch('http://localhost:5201/api/plane/get-all',
-        {credentials: 'include'})
-            .then((response) => response.json())
-            .then((data) => data) as Array<Plane>;
+      const planes = await fetch('http://localhost:5201/api/plane/get-all',
+      {credentials: 'include'})
+        .then((response) => response.json())
+        .then((data) => data) as Array<Plane>;
 
-        setPlanes(planes); 
+      setPlanes(planes); 
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
   }
 
+  // Instructor Data Retrieval and Storage
+  const [instructors, setInstructors] = useState<Instructor[]>([]);
   const fetchInstructors = async () => {
     try {
-        const instructors = await fetch('http://localhost:5201/api/instructor/get-all',
-        {credentials: 'include'})
-            .then((response) => response.json())
-            .then((data) => data) as Array<Instructor>;
+      const instructors = await fetch('http://localhost:5201/api/instructor/get-all',
+      {credentials: 'include'})
+        .then((response) => response.json())
+        .then((data) => data) as Array<Instructor>;
 
-        setInstructors(instructors); 
+      setInstructors(instructors); 
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
   }
 
@@ -106,49 +104,75 @@ function Aircraft() {
     fetchInstructors();
   }, []); 
 
-          const customStyles = {
-            paper: {
-              backgroundColor: '#D3D3D3',
-              borderRadius: '12px',
-              padding: '16px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: 'fit-content',
-            
-            },
-          };
+  const gridStyle = {
+    paper: {
+      backgroundColor: '#DDDDDD',
+      borderRadius: '12px',
+      padding: '16px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 'fit-content',
+    }
+  };
+
+  const buttonStyle = {
+    paper: {
+      variant: "h4"
+    }
+  }
         
   return (
     <div className="aircraft-page">
-        <StaticSidebar />
-        <div className="page-content">
-          <div className="filters">
-        
-          </div>
-
-          <div className="images" key={ updateScreen.toString()}>
-            <Grid container spacing={2}>
-              {planes.map((plane, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index} >
-                <Paper elevation={3} style={{...customStyles.paper, flexDirection: 'column', flexWrap: 'nowrap'}}>
-                  <Button fullWidth onClick={()=>{handleClickOpen(plane)}}>  
-                    <Typography variant="h4" align="center">
-                      {plane.nickName}
-                    </Typography>
-                    <Typography align="center">
-                      {`${plane.model}, ${plane.grounded ? 'Grounded' : 'In Service'}`}
-                    </Typography>
-                  </Button>
-                </Paper>
-              </Grid>
+      <StaticSidebar />
+      <div className="page-content">
+        <div className="aircraft-page-header">
+          {/*<p>Aircraft</p>*/}
+        </div>
+        <div className="images" key={ updateScreen.toString()}>
+          {/* MaterialUI Grid of Planes*/}
+          <Grid container spacing={2}>
+            {planes.map((plane, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index} >
+              <Paper elevation={2} style={{...gridStyle.paper, flexDirection: 'column', flexWrap: 'nowrap'}}>
+                <Button fullWidth onClick={()=>{handleClickPlane(plane)}}>
+                  <Typography 
+                    variant="h4" 
+                    align="center" 
+                    fontFamily={"Segoe UI"}>
+                    {`${plane.nickName} [${plane.tailNumber}]`}
+                  </Typography>
+                </Button>
+              </Paper>
+            </Grid>
             ))}
           </Grid>
-          <AircraftPopup open={open} onClose={handleClose} plane={currentPlane} openSquawk={handleClickOpenSquawk} openCreateReservation={handleClickOpenCreateReservation} openModify={handleClickOpenModify}/>
-          <AddSqauwkPopup open={openSquawk} onClose={handleCloseSquawk} plane={currentPlane}/>
-          <NewReservation open={openCreateReservation} onClose={handleCloseCreateReservation} Planes={planes} Instructors={instructors} SelectedPlane={currentPlane}/>
-          <ModifyAircraft open={openModify} onClose={handleCloseModify} planeId={currentPlane.planeId} updateScreen={handleUpdateScreen} setCurrentPlane={setCurrentPlane}/>
+
+          {/* Popup Components */}
+          <AircraftPopup 
+            open={openAircraftPage} 
+            onClose={handleCloseAircraftPage} 
+            plane={currentPlane} 
+            openSquawk={handleClickOpenSquawk} 
+            openCreateReservation={handleClickOpenCreateReservation} 
+            openModify={handleClickOpenModify}/>
+          <AddSqauwkPopup 
+            open={openAddSquawk} 
+            onClose={handleCloseSquawk} 
+            plane={currentPlane}/>
+          <NewReservation 
+            open={openCreateReservation} 
+            onClose={handleCloseCreateReservation} 
+            Planes={planes} 
+            Instructors={instructors} 
+            SelectedPlane={currentPlane}/>
+          <ModifyAircraft 
+            open={openModifyPlane} 
+            onClose={handleCloseModify} 
+            planeId={currentPlane.planeId} 
+            updateScreen={handleUpdateScreen} 
+            setCurrentPlane={setCurrentPlane}/>
         </div>
       </div>       
     </div>      
