ba49bb604a5032847c7f3d81da18629d1c867e7e
Zachary Martin <zachary1516@gmail.com>
2023-11-08 18:12:17 -0500

    changed mxId to squawkId

--- src/Home/AllSquawks/AllSquawks.tsx
+++ src/Home/AllSquawks/AllSquawks.tsx
@@ -4,107 +4,122 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import SquawkInfo from "./SquawkInfo";
 
 export interface AllSquawkProps {
-    planes: Array<Plane>
+  planes: Array<Plane>;
 }
 
 function AllSquawks(props: AllSquawkProps) {
-    // Define Columns of Squawk Grid
-    const columns: GridColDef[] = [
-        {
-            field: 'date_opened',
-            headerName: 'Date Opened',
-            type: 'string',
-            editable: false,
-            width: 160,
-        },
-        {
-            field: 'plane_name',
-            headerName: 'Plane',
-            type: 'string',
-            editable: true,
-            width: 240,
-        },
-        {
-            field: 'description',
-            headerName: 'Description',
-            type: 'string',
-            editable: true,
-            width: 640,
-        },
-        {
-            field: 'type',
-            headerName: 'Type',
-            type: 'singleSelect',
-            editable: true,
-            width: 128,
-            valueOptions: ["Planned", "Unplanned", "100 Hour", "Annual"]
-        }
-    ];
+  // Define Columns of Squawk Grid
+  const columns: GridColDef[] = [
+    {
+      field: "date_opened",
+      headerName: "Date Opened",
+      type: "string",
+      editable: false,
+      width: 160,
+    },
+    {
+      field: "plane_name",
+      headerName: "Plane",
+      type: "string",
+      editable: true,
+      width: 240,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      type: "string",
+      editable: true,
+      width: 640,
+    },
+    {
+      field: "type",
+      headerName: "Type",
+      type: "singleSelect",
+      editable: true,
+      width: 128,
+      valueOptions: ["Planned", "Unplanned", "100 Hour", "Annual"],
+    },
+  ];
 
-    // Get Squawks
-    const SquawkLabel = [{label: "Planned", value: 1}, {label: "Unplanned", value: 2}, {label: "100 Hour", value: 3}, {label: "Annual", value: 4}]
-    const [rows, setRows] = useState([]);
-    const getSquawks = async () => {
-        try {
-            if (props.planes.length > 0) {
-                const squawks = await fetch(`http://localhost:5201/api/squawks/get-all`,
-                {credentials: 'include'})
-                    .then((response) => response.json())
-                    .then((data) => data);
-                const mappedRows = squawks.map((squawk: any, index: number) => {
-                    const date = new Date(squawk.dateOpened);
-                    return {
-                        id: index,
-                        mxId: squawk.mxId,
-                        plane_name: `${props.planes.find(o => o.planeId === squawk.planeId)?.nickName} 
-                        - [${props.planes.find(o => o.planeId === squawk.planeId)?.tailNumber}]`,
-                        date_opened: `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`,
-                        description: squawk.description,
-                        type: SquawkLabel.find((object) => object.value == squawk.type)
-                    };
-                });
-                setRows(mappedRows);
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  // Get Squawks
+  const SquawkLabel = [
+    { label: "Planned", value: 1 },
+    { label: "Unplanned", value: 2 },
+    { label: "100 Hour", value: 3 },
+    { label: "Annual", value: 4 },
+  ];
+  const [rows, setRows] = useState([]);
+  const getSquawks = async () => {
+    try {
+      if (props.planes.length > 0) {
+        const squawks = await fetch(
+          `http://localhost:5201/api/squawks/get-all`,
+          { credentials: "include" }
+        )
+          .then((response) => response.json())
+          .then((data) => data);
+        const mappedRows = squawks.map((squawk: any, index: number) => {
+          const date = new Date(squawk.dateOpened);
+          return {
+            id: index,
+            squawkId: squawk.squawkId,
+            plane_name: `${props.planes.find(
+              (o) => o.planeId === squawk.planeId
+            )?.nickName} 
+                        - [${props.planes.find(
+                          (o) => o.planeId === squawk.planeId
+                        )?.tailNumber}]`,
+            date_opened: `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`,
+            description: squawk.description,
+            type: SquawkLabel.find((object) => object.value == squawk.type),
+          };
+        });
+        setRows(mappedRows);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        getSquawks();
-    }, [props.planes]);
-    
-    const [openPopup, setOpenPopup] = useState(false);
-    const [currSquawk, setCurrSquawk] = useState("");
-    const handleRowClick = (clicked: any) => {
-        setCurrSquawk(clicked.row.mxId);
-        setOpenPopup(true);
-    }
+  useEffect(() => {
+    getSquawks();
+  }, [props.planes]);
 
-    const handleClosePopup = () => {
-        setOpenPopup(false)
-    }
+  const [openPopup, setOpenPopup] = useState(false);
+  const [currSquawk, setCurrSquawk] = useState("");
+  const handleRowClick = (clicked: any) => {
+    setCurrSquawk(clicked.row.squawkId);
+    setOpenPopup(true);
+  };
 
-    return(
-        <div>
-            <DataGrid
-                sx={{width: "90em", m: 2 }}
-                rows={rows}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: {
-                            pageSize: 5,
-                        },
-                    },
-                }}
-                autoHeight
-                disableRowSelectionOnClick
-                onRowClick={handleRowClick}
-            />
-            <SquawkInfo open={openPopup} onClose={handleClosePopup} squawkId={currSquawk} />
-        </div>
-    )
+  const handleClosePopup = () => {
+    setOpenPopup(false);
+  };
+
+  return (
+    <div>
+      <DataGrid
+        sx={{ width: "90em", m: 2 }}
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        autoHeight
+        disableRowSelectionOnClick
+        onRowClick={handleRowClick}
+      />
+      <SquawkInfo
+        open={openPopup}
+        onClose={handleClosePopup}
+        squawkId={currSquawk}
+      />
+    </div>
+  );
 }
 
 export default AllSquawks;
