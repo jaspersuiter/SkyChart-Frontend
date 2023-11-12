d74f9954c0c790ad1246874742c031ac24d74d81
Zachary Martin <zachary1516@gmail.com>
2023-11-12 09:01:56 -0500

    changed api endpoints and params to match

--- src/Admin/InviteNewUser/InviteNewUser.tsx
+++ src/Admin/InviteNewUser/InviteNewUser.tsx
@@ -1,12 +1,11 @@
+import { useNavigate } from "react-router-dom";
 
-import { useNavigate } from 'react-router-dom';
-
-import TextField from '@mui/material/TextField';
-import PrimaryButton from '../../Utils/Buttons/PrimaryButton';
-import { Dialog } from '@mui/material';
-import CancelButton from '../../Utils/Buttons/CancelButton';
-import React from 'react';
-import { makeApiCall } from '../../APICall';
+import TextField from "@mui/material/TextField";
+import PrimaryButton from "../../Utils/Buttons/PrimaryButton";
+import { Dialog } from "@mui/material";
+import CancelButton from "../../Utils/Buttons/CancelButton";
+import React from "react";
+import { makeApiCall } from "../../APICall";
 
 export interface InviteNewUserProps {
   open: boolean;
@@ -14,9 +13,9 @@ export interface InviteNewUserProps {
 }
 
 function InviteNewUser(props: InviteNewUserProps) {
-  const [email, setEmail] = React.useState('');
-  const {open, onClose } = props;
-  const [errormessage, setErrormessage] = React.useState('')
+  const [email, setEmail] = React.useState("");
+  const { open, onClose } = props;
+  const [errormessage, setErrormessage] = React.useState("");
 
   const handleClose = () => {
     onClose();
@@ -24,53 +23,71 @@ function InviteNewUser(props: InviteNewUserProps) {
 
   const handleLogOut = () => {
     onClose();
-  }
+  };
 
   const resetAll = () => {
-    setEmail('');
-    setErrormessage('');
+    setEmail("");
+    setErrormessage("");
     handleClose();
-  }
+  };
 
   const createNewUser = async () => {
     const params = {
       email: email,
-    }
+    };
     try {
-        const responseData2 = await makeApiCall("/api/user/create", {}, "post", params)
+      const responseData2 = await makeApiCall(
+        "/api/register/register-user",
+        params,
+        "post"
+      );
 
-        if (responseData2 === "User already exists"){
-          setErrormessage(responseData2)
-          return
-          } else if(responseData2 === "Invalid Email"){
-          setErrormessage(responseData2)
-          return
-        }
-        resetAll();
-    } catch (error) { 
+      if (responseData2 === "User already exists") {
+        setErrormessage(responseData2);
+        return;
+      } else if (responseData2 === "Invalid Email") {
+        setErrormessage(responseData2);
+        return;
+      }
+      resetAll();
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-    return (
-      <div className="invite-new-user-popup">
-        <Dialog onClose={handleClose} open={open}>
-          <h2>SkyChart</h2>
-          <p>Invite an instructor, student, or pilot to join SkyChart</p>
-          <form>
-            <TextField id="email" label="Email" value={email} onChange={(event) => {setEmail(event.target.value)}} required />
-          </form>
-          <br/>
-          <div className='error-message'>
-            {errormessage}
-          </div>
-          <div className='invite-new-user-buttons' style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '16px' }}>
-            <PrimaryButton text="Send Invatation" onClick={createNewUser}/>
-            <CancelButton text="Cancel" onClick={handleClose}/>
-          </div>
-        </Dialog>
-      </div>
-    );
+  return (
+    <div className="invite-new-user-popup">
+      <Dialog onClose={handleClose} open={open}>
+        <h2>SkyChart</h2>
+        <p>Invite an instructor, student, or pilot to join SkyChart</p>
+        <form>
+          <TextField
+            id="email"
+            label="Email"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            required
+          />
+        </form>
+        <br />
+        <div className="error-message">{errormessage}</div>
+        <div
+          className="invite-new-user-buttons"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: "16px",
+          }}
+        >
+          <PrimaryButton text="Send Invatation" onClick={createNewUser} />
+          <CancelButton text="Cancel" onClick={handleClose} />
+        </div>
+      </Dialog>
+    </div>
+  );
 }
 
 export default InviteNewUser;
--- src/Login/NewPassword/NewPassword.tsx
+++ src/Login/NewPassword/NewPassword.tsx
@@ -1,55 +1,69 @@
-import './NewPassword.css';
+import "./NewPassword.css";
 
-import TextField from '@mui/material/TextField';
-import PrimaryButton from '../../Utils/Buttons/PrimaryButton';
-import { useContext, useState } from 'react';
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
-import React from 'react';
-
-
+import TextField from "@mui/material/TextField";
+import PrimaryButton from "../../Utils/Buttons/PrimaryButton";
+import { useContext, useState } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import React from "react";
+import { env } from "../../env";
 
 function NewPassword() {
-
-  const [password, setPassword] = useState('');
-  const  params  = new URLSearchParams(window.location.search);
+  const [password, setPassword] = useState("");
+  const params = new URLSearchParams(window.location.search);
   const navigate = useNavigate();
-  
+
   const handleNewPassword = () => {
-    fetch(`http://localhost:5201/api/user/password/reset?userId=${params.get('userId')}`, {credentials: 'include', method: 'PUT', headers: {'Content-Type': 'application/json'} , body: JSON.stringify({newPassword: password})})
-      .then(response => response.json())
-      .then(data => {
-        
-       
-          console.log('Success:', data);
-          navigate('/login');
- 
-        
+    fetch(
+      `${env.SKYCHART_API_URL}/api/password/reset?token=${params.get("token")}`,
+      {
+        credentials: "include",
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ newPassword: password }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        navigate("/login");
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-  }
+  };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
-  }
+  };
 
   return (
     <div className="new-password-page">
       <div className="new-password-form">
-        <div className='TitleBar'>
-          <h2 style={{fontSize: 35}}>SkyChart</h2>
+        <div className="TitleBar">
+          <h2 style={{ fontSize: 35 }}>SkyChart</h2>
         </div>
-        <div className='TextFieldBar'>
-          <h5 style={{fontSize: 18}}>Enter New Password</h5>
+        <div className="TextFieldBar">
+          <h5 style={{ fontSize: 18 }}>Enter New Password</h5>
         </div>
         <div className="FlexColumnItem">
-          <TextField id="password" label="Password" type="password" required value={password} onChange={handlePasswordChange} fullWidth />
-          <br/>
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            required
+            value={password}
+            onChange={handlePasswordChange}
+            fullWidth
+          />
+          <br />
         </div>
-        <br/>
+        <br />
         <div className="FlexColumnItem">
-          <PrimaryButton text="Reset Password" onClick={handleNewPassword} width="100%" />
+          <PrimaryButton
+            text="Reset Password"
+            onClick={handleNewPassword}
+            width="100%"
+          />
         </div>
       </div>
     </div>
