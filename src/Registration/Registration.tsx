a2853f28e8a3b5efd1fd923ff803b2309a629e37
jaspersuiter <jaspersuiter@gmail.com>
2023-11-16 22:42:55 -0500

    regex for only allowing phone numbers ins ########## format

--- src/Registration/Registration.tsx
+++ src/Registration/Registration.tsx
@@ -22,7 +22,7 @@ function Registration() {
 
   const validpassword = password === confirm_password;
   const isValidPhoneNumber = (phoneNumber: string) => {
-    const phoneRegex = /^\d{9}$/;
+    const phoneRegex = /^\d{10}$/;
 
     return phoneRegex.test(phoneNumber);
   };
@@ -50,6 +50,13 @@ function Registration() {
   };
 
   const registerUser = async () => {
+    if (!isValidPhoneNumber(phone) || !isValidPhoneNumber(ec_phone)) {
+      setErrormessage(
+        "Invalid phone number format. Please enter a valid 10-digit number."
+      );
+      return;
+    }
+
     const data = {
       username: userName,
       email: email,
