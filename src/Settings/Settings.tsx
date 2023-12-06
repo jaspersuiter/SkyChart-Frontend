f9dbe788f2e7dd456f7523dba45a4e34c90e4106
Alex Szilagyi <amszilag@purdue.edu>
2023-12-06 13:48:16 -0500

    User can now update profile and all settings page fields are prepopulated with data from DB

--- src/Settings/Settings.tsx
+++ src/Settings/Settings.tsx
@@ -63,7 +63,8 @@ interface User {
   username: string;
   address: string;
   emergencyContactName: string;
-  emergencyContactPhone: string;
+  emergencyContactPhoneNumber: string;
+  preferredInstructorId: string;
   preferredInstructor: string;
   preferredPlanes: string[];
   proficientPlaneModels: string[];
@@ -71,7 +72,8 @@ interface User {
 
 function Settings() {
   const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [emergencyContactName, setEmergencyContactName] = useState("");
   const [mailingAddress, setMailingAddress] = useState("");
   const [emergencyContactPhoneNumber, setEmergencyContactPhoneNumber] = useState("");
@@ -81,10 +83,13 @@ function Settings() {
   const [planes, setPlanes] = useState<Plane[]>([]);
   const [instructors, setInstructors] = useState<Instructor[]>([]); // Declare rows as a state variable
   const [openPreferenceConfirm, setOpenPreferenceConfirm] = useState(false);
+  const [openUpdateUserConfirm, setOpenUpdateUserConfirm] = useState(false);
+  const [errormessage, setErrormessage] = useState("");
+
 
   const resetAll = () => {
-    setSelectedInstructor("");
-    setSelectedAircraft([]);
+    //setSelectedInstructor("");
+    //setSelectedAircraft([]);
   };
 
   const closePreferenceConfirmDialog = () => {
@@ -95,12 +100,24 @@ function Settings() {
     setOpenPreferenceConfirm(true);
   };
 
+  const closeUpdateUserConfirmDialog = () => {
+    resetAll();
+    setOpenUpdateUserConfirm(false);
+  };
+  const openUpdateUserConfirmDialog = () => {
+    setOpenUpdateUserConfirm(true);
+  };
+
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
   };
 
   const handleEmergencyContactNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -180,13 +197,49 @@ function Settings() {
     }
   };
 
+  const populatePreferredInstructor = () => {
+    const foundInstructor = instructors.find(
+      (instructor) => instructor.id.toString() === user?.preferredInstructorId
+    );
+  
+    if (foundInstructor) {
+      setSelectedInstructor(user?.preferredInstructorId.toString() ?? "");
+    }
+  };
+
+  const populatePreferredAircrafts = () => {
+    setSelectedAircraft(user?.preferredPlanes ?? []);
+  };
+
+  const populateUserFields = () => {
+    setEmail(user?.email ?? "");
+    setEmergencyContactName(user?.emergencyContactName ?? "");
+    setEmergencyContactPhoneNumber(user?.emergencyContactPhoneNumber ?? "");
+    setFirstName(user?.firstName ?? "");
+    setLastName(user?.lastName ?? "");
+    setMailingAddress(user?.address ?? "");
+  }
+
   useEffect(() => {
     fetchInstructors();
     fetchPlanes();
     getUser();
-    setSelectedInstructor(user?.preferredInstructor ?? "");
-    setSelectedAircraft(user?.preferredPlanes ?? []);
   }, []);
+
+  useEffect(() => {
+    populatePreferredInstructor();
+    populatePreferredAircrafts();
+    populateUserFields();
+  }, [instructors, 
+    user?.preferredInstructorId, 
+    user?.preferredPlanes, 
+    user?.email, 
+    user?.emergencyContactName, 
+    user?.emergencyContactPhoneNumber, 
+    user?.firstName, 
+    user?.lastName, 
+    user?.address
+  ]);
 
   const fetchInstructors = async () => {
     try {
@@ -215,143 +268,209 @@ function Settings() {
     }
   };
 
+  const updateUser = async () => {
+    const data = {
+      Email: email,
+      FirstName: firstName,
+      LastName: lastName,
+      EmergencyContactName: emergencyContactName,
+      EmergencyContactPhoneNumber: emergencyContactPhoneNumber,
+      Address: mailingAddress,
+    };
+
+    let responseData2 = null;
+    try {
+      responseData2 = await makeApiCall(
+        "/api/user/update",
+        data,
+        "put"
+      );
+
+      console.log(responseData2);
+
+      if (
+        responseData2 ===
+        "Couldn't find UserId"
+      ) {
+        setErrormessage(responseData2);
+        return;
+      } else if (responseData2 === "Cannot have more than 3 preferred planes") {
+        setErrormessage(responseData2);
+        return;
+      } else if (responseData2 === "Cannot have duplicate planes") {
+        setErrormessage(responseData2);
+        return;
+      } else if (responseData2 === "At least one of the preferred planes don't exist in the database") {
+        setErrormessage(responseData2);
+        return;
+      }
+      resetAll();
+    } catch (error) {
+      console.error(error);
+    }
+
+  };
+
   return (
     <div className="settings-content">
       <StaticSidebar />
         <div className="row-items">
           <div className="column-items">
             <div className="preferences-box">
-              <h1 className="h1">Set Preferences</h1>
-              {/*<p>
-                Proficient Plane Models:{" "}
-                <b>{user?.proficientPlaneModels.join(", ")}</b>
-              </p>*/}
-              <FormControl sx={{ minWidth: 300 }} size="small">
-                <InputLabel id="preferred-instructor-label">
-                  Preferred Instructor
-                </InputLabel>
-                <Select
-                  labelId="instructor-label"
-                  id="instructor-select"
-                  label="instructor"
-                  value={selectedInstructor} // Controlled value
-                  onChange={(e) => setSelectedInstructor(e.target.value)}
-                >
-                  {instructors.map((instructor) => (
-                    <MenuItem key={instructor.id} value={instructor.id}>
-                      {instructor.firstName} {instructor.lastName}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <FormControl sx={{ minWidth: 300, maxWidth: 300 }} size="small">
-                <InputLabel id="preferred-aircraft-label">
-                  Preferred Aircraft
-                </InputLabel>
-                <Select
-                  multiple
-                  labelId="aircraft-label"
-                  id="aircraft-select"
-                  label="aircraft"
-                  value={selectedAircraft}
-                  onChange={handleChange}
-                  renderValue={(selected) => {
-                    if (Array.isArray(selected)) {
-                      return selected
-                        .map((planeId) => {
-                          const plane = planes.find(
-                            (p) => `${p.id}` === `${planeId}`
-                          );
-                          return plane
-                            ? `${plane.model} (${plane.nickname})`
-                            : "";
-                        })
-                        .join(", ");
-                    }
-                    return "";
-                  }}
-                >
-                  {planes.map((plane) => (
-                    <MenuItem
-                      key={plane.id}
-                      value={plane.id}
-                      sx={{ justifyContent: "space-between" }}
+              <div className="column-flex-item">
+                <h1 className="h1">Set Preferences</h1>
+                {/*<p>
+                  Proficient Plane Models:{" "}
+                  <b>{user?.proficientPlaneModels.join(", ")}</b>
+                </p>*/}
+                <div className="row-flex-item">
+                  <FormControl sx={{ minWidth: 300 }} size="small">
+                    <InputLabel id="preferred-instructor-label">
+                      Preferred Instructor
+                    </InputLabel>
+                    <Select
+                      labelId="instructor-label"
+                      id="instructor-select"
+                      label="instructor"
+                      value={selectedInstructor} // Controlled value
+                      onChange={(e) => setSelectedInstructor(e.target.value)}
                     >
-                      {`${plane.model} (${plane.nickname})`}
-                      <Checkbox
-                        checked={selectedAircraft.indexOf(`${plane.id}`) > -1}
-                      />
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <PrimaryButton
-                text="Confirm Changes"
-                onClick={openPreferenceConfirmDialog}
-              />
-              <ConfirmPopup
-                open={openPreferenceConfirm}
-                onClose={closePreferenceConfirmDialog}
-                func={updatePreferredItems}
-                text="Are you sure you want add these preferences?"
-              />
+                      {instructors.map((instructor) => (
+                        <MenuItem key={instructor.id} value={instructor.id}>
+                          {instructor.firstName} {instructor.lastName}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormControl sx={{ minWidth: 300, maxWidth: 300 }} size="small">
+                    <InputLabel id="preferred-aircraft-label">
+                      Preferred Aircraft
+                    </InputLabel>
+                    <Select
+                      multiple
+                      labelId="aircraft-label"
+                      id="aircraft-select"
+                      label="aircraft"
+                      value={selectedAircraft}
+                      onChange={handleChange}
+                      renderValue={(selected) => {
+                        if (Array.isArray(selected)) {
+                          return selected
+                            .map((planeId) => {
+                              const plane = planes.find(
+                                (p) => `${p.id}` === `${planeId}`
+                              );
+                              return plane
+                                ? `${plane.model} (${plane.nickname})`
+                                : "";
+                            })
+                            .join(", ");
+                        }
+                        return "";
+                      }}
+                    >
+                      {planes.map((plane) => (
+                        <MenuItem
+                          key={plane.id}
+                          value={plane.id}
+                          sx={{ justifyContent: "space-between" }}
+                        >
+                          {`${plane.model} (${plane.nickname})`}
+                          <Checkbox
+                            checked={selectedAircraft.indexOf(`${plane.id}`) > -1}
+                          />
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </div>
+                <PrimaryButton
+                  text="Confirm Changes"
+                  onClick={openPreferenceConfirmDialog}
+                />
+                <ConfirmPopup
+                  open={openPreferenceConfirm}
+                  onClose={closePreferenceConfirmDialog}
+                  func={updatePreferredItems}
+                  text="Are you sure you want add these preferences?"
+                />
+              </div>
             </div>
             <div className="availability-box">
-              <h1 className="h1">Set Availability</h1>
-              <InstructorAvailibility />
+              <div className="column-flex-item">
+                <h1 className="h1">Set Availability</h1>
+                <InstructorAvailibility />
+              </div>
             </div>
           </div>
           <div className="profile-box">
-            <h1 className="h1">Edit Profile</h1>
-            <TextField
-              id="email"
-              label="Email"
-              type="email"
-              required
-              value={email}
-              onChange={handleEmailChange}
-              fullWidth
-            />
-            <TextField
-              id="name"
-              label="Name"
-              type="name"
-              required
-              value={name}
-              onChange={handleNameChange}
-              fullWidth
-            />
-            <TextField
-              id="emergencyContactName"
-              label="Emergency Contact Name"
-              type="emergencyContactName"
-              required
-              value={emergencyContactName}
-              onChange={handleEmergencyContactNameChange}
-              fullWidth
-            />
-            <TextField
-              id="mailingAddress"
-              label="Mailing Address"
-              type="email"
-              required
-              value={mailingAddress}
-              onChange={handleMailingAddressChange}
-              fullWidth
-            />
-            <TextField
-              id="emergencyContactPhoneNumber"
-              label="Emergency Contact Phone Number"
-              type="emergencyContactPhoneNumber"
-              required
-              value={emergencyContactPhoneNumber}
-              onChange={handleEmergencyContactPhoneNumberChange}
-              fullWidth
-            />
-            <PrimaryButton
-                text="Confirm Changes"
-                onClick={openPreferenceConfirmDialog}
-            />
+            <div className="column-flex-item">
+              <h1 className="h1">Edit Profile</h1>
+              <TextField
+                id="email"
+                label="Email"
+                type="email"
+                required
+                value={email}
+                onChange={handleEmailChange}
+                fullWidth
+              />
+              <TextField
+                id="firstName"
+                label="First Name"
+                type="firstName"
+                required
+                value={firstName}
+                onChange={handleFirstNameChange}
+                fullWidth
+              />
+              <TextField
+                id="lastName"
+                label="Last Name"
+                type="lastName"
+                required
+                value={lastName}
+                onChange={handleLastNameChange}
+                fullWidth
+              />
+              <TextField
+                id="emergencyContactName"
+                label="Emergency Contact Name"
+                type="emergencyContactName"
+                required
+                value={emergencyContactName}
+                onChange={handleEmergencyContactNameChange}
+                fullWidth
+              />
+              <TextField
+                id="mailingAddress"
+                label="Mailing Address"
+                type="email"
+                required
+                value={mailingAddress}
+                onChange={handleMailingAddressChange}
+                fullWidth
+              />
+              <TextField
+                id="emergencyContactPhoneNumber"
+                label="Emergency Contact Phone Number"
+                type="emergencyContactPhoneNumber"
+                required
+                value={emergencyContactPhoneNumber}
+                onChange={handleEmergencyContactPhoneNumberChange}
+                fullWidth
+              />
+              <PrimaryButton
+                  text="Confirm Changes"
+                  onClick={openUpdateUserConfirmDialog}
+              />
+              <ConfirmPopup
+                  open={openUpdateUserConfirm}
+                  onClose={closeUpdateUserConfirmDialog}
+                  func={updateUser}
+                  text="Are you sure you want apply these updates?"
+              />
+            </div>
           </div>
         </div>
       </div>
